d32473898e223678636855d61860b5205b8a17ed
AlejandroFelipe Acuna <afacunam@gmail.com>
2026-09-17 11:42:50 +0200

    feat: basic lottery logic implemented without history

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 "use strict";
 
+const history = [  {
+    userNumber: 4,
+    randomNumber: 7,
+    result: "Perdiste"
+  }];
+
 
 const input = document.querySelector("#input-id");
 
@@ -25,7 +31,34 @@ form.addEventListener("submit", function (event) {
 
     const randomNumber = getRandomNumber();
 
-    console.log(randomNumber)
+    console.log(randomNumber);
+
+    // Comparison
+
+    const isEqual = compareNumbers(userNumber, randomNumber);
+
+    let result = "";
+
+    if (isEqual) {
+      result = "Ganaste"
+      document.querySelector("#result-id").innerText = `${result}`;
+      document.querySelector("#message-id").innerText = `El número aleatorio generado fue: ${randomNumber}`;
+    // Add the successful try
+      const newTry = {
+        userNumber,
+        randomNumber,
+        result
+      }
+
+      history.push(newTry);
+
+    // Show History (to be implemented): showHistory(newTry)
+
+    } else {
+      result = "Perdiste"
+      document.querySelector("#result-id").innerText = `${result}`;
+      document.querySelector("#message-id").innerText = `El número aleatorio generado fue: ${randomNumber}`;
+    }
 
   }
   
@@ -64,6 +97,26 @@ function compareNumbers(userNumber, randomNumber) {
   }
 
 }
+
+console.log(history)
+
+// showHistory() to be implemented;
+
+/*function showHistory(newTry) {
+
+const ul = document.querySelector("#history-id");
+const li = document.createElement("li");
+
+li.textContent = newTry.userNumber;
+  ul.appendChild(li);
+       
+}
+
+
+let x = showHistory(history);
+console.log("Lo que debería mostrar:", x)
+
+*/
 
 
 
