97194d2848fb7084b410a7e86228148dfc919863
AlejandroFelipe Acuna <afacunam@gmail.com>
2026-09-17 10:05:38 +0200

    feat(main) create initial JS logic structure

---
+++ main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+const input = document.querySelector("#input-id");
+
+const form = document.querySelector("#form-id");
+
+form.addEventListener("submit", function (event) {
+
+  event.preventDefault();
+
+  const userInput = input.value;
+
+  
+
+})
