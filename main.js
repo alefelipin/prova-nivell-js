3e68c22f3c93384b999e447415e3173a980323be
AlejandroFelipe Acuna <afacunam@gmail.com>
2026-09-17 10:26:16 +0200

    feat: implement validation function

--- main.js
+++ main.js
@@ -9,8 +9,34 @@ form.addEventListener("submit", function (event) {
 
   event.preventDefault();
 
-  const userInput = input.value;
+  const userInput = parseInt(input.value);
 
+  // Validación
+
+  const isValid = validateNumber(userInput);
+
+  if(!isValid) {
+    document.querySelector("#message-id").innerText = "No es válido";
+  }
+
+  if(isValid) {
+    console.log("es válido")
+  }
   
 
-})
+});
+
+function validateNumber(number) {
+
+  if( number < 1 || number > 10 ) {
+    return false;
+  }
+
+  if ( isNaN(number)) {
+    return false;
+  }
+
+  return true;
+
+}
+
